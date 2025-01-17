c2d59d3754d291ad9352a8b8d54d4a7ec36d0de2
edymusajev <edymusajev@gmail.com>
2025-01-17 15:31:54 +0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
+import {
+  Calendar,
+  Files,
+  FileSearch,
+  GitGraph,
+  LayoutDashboard,
+  Network,
+  Terminal,
+  Trees,
+} from "lucide-react";
 import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1 className="bg-red-400">
-        A rare working Electron + Vite + React template
-      </h1>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <p>You clicked {count} times</p>
+    // main layout
+    <div className="h-screen w-screen flex">
+      {/* left panel */}
+      <div className="h-screen w-80 bg-stone-50 border-r flex-none">
+        <div className="h-10 w-full border-b drag bg-stone-50"></div>
+        <div className="w-12 h-full border-r flex flex-col items-center gap-1.5 py-2">
+          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+            <FileSearch
+              size={18}
+              strokeWidth={1.8}
+              className="text-stone-500"
+            />
+          </button>
+          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+            <Network size={18} strokeWidth={1.8} className="text-stone-500" />
+          </button>
+          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+            <LayoutDashboard
+              size={18}
+              strokeWidth={1.8}
+              className="text-stone-500"
+            />
+          </button>
+          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+            <Calendar size={18} strokeWidth={1.8} className="text-stone-500" />
+          </button>
+          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+            <Files size={18} strokeWidth={1.8} className="text-stone-500" />
+          </button>
+          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+            <Terminal size={18} strokeWidth={1.8} className="text-stone-500" />
+          </button>
+        </div>
+      </div>
+      <div className="h-10 w-full border-b drag bg-stone-50"></div>
     </div>
   );
 }
