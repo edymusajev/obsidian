6b2f8f2acfd15a7b0ea2e1d20dc1c197c9b75360
edymusajev <edymusajev@gmail.com>
2025-01-18 16:58:18 +0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -11,46 +11,43 @@ import {
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     // main layout
     <div className="h-screen w-screen flex">
-      {/* left panel */}
-      <div className="h-screen w-80 bg-stone-50 border-r flex-none">
+      {/* workspace ribbon */}
+      <div className="before:content-[''] before:absolute before:top-0 before:left-0 before:w-11 before:border-b before:h-10 before-w-11 before:bg-stone-50  flex-none h-full w-11 flex flex-col items-center gap-1.5 mt-10 border-r"></div>
+
+      <div className=" w-full h-full">
+        {/* tab header container */}
         <div className="h-10 w-full border-b drag bg-stone-50"></div>
-        <div className="w-12 h-full border-r flex flex-col items-center gap-1.5 py-2">
-          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
-            <FileSearch
-              size={18}
-              strokeWidth={1.8}
-              className="text-stone-500"
-            />
-          </button>
-          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
-            <Network size={18} strokeWidth={1.8} className="text-stone-500" />
-          </button>
-          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
-            <LayoutDashboard
-              size={18}
-              strokeWidth={1.8}
-              className="text-stone-500"
-            />
-          </button>
-          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
-            <Calendar size={18} strokeWidth={1.8} className="text-stone-500" />
-          </button>
-          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
-            <Files size={18} strokeWidth={1.8} className="text-stone-500" />
-          </button>
-          <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
-            <Terminal size={18} strokeWidth={1.8} className="text-stone-500" />
-          </button>
-        </div>
       </div>
-      <div className="h-10 w-full border-b drag bg-stone-50"></div>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+          <FileSearch size={18} strokeWidth={1.8} className="text-stone-500" />
+        </button>
+        <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+          <Network size={18} strokeWidth={1.8} className="text-stone-500" />
+        </button>
+        <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+          <LayoutDashboard
+            size={18}
+            strokeWidth={1.8}
+            className="text-stone-500"
+          />
+        </button>
+        <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+          <Calendar size={18} strokeWidth={1.8} className="text-stone-500" />
+        </button>
+        <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+          <Files size={18} strokeWidth={1.8} className="text-stone-500" />
+        </button>
+        <button className="hover:bg-stone-200 px-1.5 py-1 rounded">
+          <Terminal size={18} strokeWidth={1.8} className="text-stone-500" />
+        </button> */
+}
